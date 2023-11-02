bb55522bbd4a56b4374c33cdcb6c68627c230fb5
Neotech Academy <academy@neotech.srl>
2023-11-02 10:57:33 +0100

    javaTest01

---
+++ java/javatest01/test01/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int x=20;
+        int q=45;
+        System.out.println(q);
+        while (x<100){
+            System.out.println(x+" è minore di 100");
+            x++;
+        }
+        System.out.println("sono uscito");
+    }
+}
